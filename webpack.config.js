8c3ba8a12a3225b2e2428c3f4b4f7546700d0c15
Finn Terdal <fterdal@gmail.com>
2019-03-21 11:13:08 -0500

    refactor to match conventions

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,17 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-    ]
+    ],
   },
   entry: './client/index.js',
   output: {
     path: path.join(__dirname, 'server/public'),
-    filename: 'app.js'
-  }
-};
+    filename: 'bundle.js',
+  },
+}
