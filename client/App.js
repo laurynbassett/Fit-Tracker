76afb43f9f04990e510adbae3d2a73073c11bb53
Finn Terdal <fterdal@gmail.com>
2019-03-21 12:00:37 -0500

    fix some linter warnings

--- client/App.js
+++ client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { 
+import {
   Header,
   Main,
 } from './components'
--- client/components/SingleWorkout.js
+++ client/components/SingleWorkout.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { SingleExercise } from './SingleExercise'
 
 import './SingleWorkout.css'
