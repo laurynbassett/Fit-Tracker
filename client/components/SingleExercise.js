3f007b78a33747306cb16294912950ecb376ef27
Finn Terdal <fterdal@gmail.com>
2019-03-21 22:14:47 -0500

    remove comment with video link

--- client/components/SingleExercise.js
+++ client/components/SingleExercise.js
@@ -1,11 +1,5 @@
 import React, { Fragment } from 'react'
 
-/**
-  Check out this video demo: https://youtu.be/19uPqaQVhLc
-  Notice how the manual DOM manipulating JS outside of React conflicts with
-  React.
- */
-
 export const SingleExercise = props => {
   const { id, name, duration, completed, description } = props.exercise
   return (
